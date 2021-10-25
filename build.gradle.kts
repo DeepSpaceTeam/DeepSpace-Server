3de2ae351a21be1b8b6331cf09b4c713b0c16073
Abubakirov Vladimir <abubakirov04@mail.ru>
2021-10-25 14:32:51 +0300

    Added redirect from http to https

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-security:2.2.5.RELEASE")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
